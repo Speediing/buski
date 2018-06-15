358c55aeaac5ebdbac3558b216b7c7ada73dfcbe
Speediing <jason.wiker@gmail.com>
2018-06-14 20:05:40 -0600

    update view model to scroll to current person

--- app/src/main/java/com/Team6/buski/quickstart/database/BuskViewModel.kt
+++ app/src/main/java/com/Team6/buski/quickstart/database/BuskViewModel.kt
@@ -2,6 +2,7 @@ package com.Team6.buski.quickstart.database
 
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.Team6.buski.quickstart.database.models.User
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
@@ -10,7 +11,8 @@ import com.google.firebase.database.DatabaseReference
 
 class BuskViewModel: ViewModel() {
    lateinit var mDatabase: DatabaseReference
-    lateinit var users: List<User>
+    lateinit var users: List<com.Team6.buski.quickstart.database.User>
+    lateinit var currentUser: com.Team6.buski.quickstart.database.User
     lateinit var map: GoogleMap
 
     fun printUsers(){
@@ -18,14 +20,21 @@ class BuskViewModel: ViewModel() {
     }
 
     fun addUsers(){
+        map.clear()
         for (user in users){
-            val latlng = user.location.split(":")
-            val local = LatLng(latlng[0].toDouble(), latlng[1].toDouble())
-            map.addMarker(MarkerOptions().position(local)
-                    .title(user.username)
-                    )
-            map.moveCamera(CameraUpdateFactory.newLatLng(local))
+            val latlng = user.location?.split(":") ?: listOf("1.0", "1.0")
+            if(user.active) {
+                val local = LatLng(latlng[0].toDouble(), latlng[1].toDouble())
+                val marker = map.addMarker(MarkerOptions().position(local)
+                        .title(user.username))
+                marker.tag = user.username
+            }
         }
+    }
+    fun scrollToCurrent(){
+        val latlng = currentUser.location?.split(":") ?: listOf("1.0", "1.0")
+        val local = LatLng(latlng[0].toDouble(), latlng[1].toDouble())
+        map.moveCamera(CameraUpdateFactory.newLatLng(local))
     }
 
 }
